bc5e1154dbc110f12e312987d02bb04c255738df
Zinibow <48457344+Zinibow@users.noreply.github.com>
2021-01-31 19:22:26 -0800

    Create variations.js

---
+++ Variations.js
@@ -0,0 +1,35 @@
+/** @param {string} name */
+export function getNumberOfChars(name) {
+    // return the number of characters in: name
+
+}
+
+export function getFirstChar(name) {
+    // return the first character of: name
+
+}
+
+export function getLastChar(name) {
+    // return the last character of: name
+
+}
+
+export function getLower(name) {
+    // return name all in lower case (example: "ABC" becomes "abc")
+
+}
+
+export function getUpper(name) {
+    // return name all in upper case (example: "abc" becomes "ABC")
+
+}
+
+export function getCapitalized(name) {
+    // return a capitalized version of name (example: "alEX" becomes "Alex")
+
+}
+
+export function getClean(name) {
+    // return name without trailing and leading space (example: " alex " becomes: "alex")
+
+}
